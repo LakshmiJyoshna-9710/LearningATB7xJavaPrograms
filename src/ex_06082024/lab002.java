1ede8e318d36c902a2e07e25ab56985e8e5b84a0
lakshmijyoshna <ljyoshna1997@gmail.com>
2024-08-19 21:57:53 +0530

    Added new changes

---
+++ src/ex_06082024/lab002.java
@@ -0,0 +1,15 @@
+package ex_06082024;
+
+import java.util.Scanner;
+
+public class lab002 {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number :");
+        int user_num = sc.nextInt();
+        System.out.println("Entered User Number : " + user_num);
+    }
+}
---
+++ src/ex_06082024/lab003.java
@@ -0,0 +1,13 @@
+package ex_06082024;
+
+import java.util.Scanner;
+
+public class lab003 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Input");
+        String input = sc.next();
+        System.out.println("Typed String is -> " + input);
+
+    }
+}
